2d7236c1502d08cbdc2b2db3574a1c51b1b414af
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-09-06 13:41:23 +0200

    Fix packrat missing packages

---
+++ PACKAGES.R
@@ -0,0 +1,44 @@
+# Install packages to a docker image with packrat
+
+# Initialize packrat ===========================================================
+## Install packrat
+utils::install.packages("packrat", repos = "https://cran.rstudio.com/")
+
+## Initialize packrat, but don't let it try to find packages to install itself
+packrat::init(
+  infer.dependencies = FALSE,
+  enter = TRUE,
+  restart = FALSE
+)
+
+# Setup repository =============================================================
+# Specify repositories so they get included in packrat.lock file
+my_repos["CRAN"] <- "https://cloud.r-project.org/"
+options(repos = my_repos)
+
+# Install packages =============================================================
+# All packages will be installed to the project-specific packrat library
+cran_packages <- c(
+  "akima",
+  "compositions",
+  "cowplot",
+  "dplyr",
+  "FactoMineR",
+  "ggdendro",
+  "ggplot2",
+  "ggrepel",
+  "ggtern",
+  "khroma",
+  "magrittr",
+  "MASS",
+  "robCompositions",
+  "tidyr"
+)
+utils::install.packages(cran_packages)
+
+# Take snapshot ================================================================
+packrat::snapshot(
+  ignore.stale = TRUE,
+  snapshot.sources = TRUE,
+  infer.dependencies = FALSE
+)
